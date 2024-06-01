4663ffb202c126d990659a8ae457febc9d18f6a0
NguyenKhaiHoan <banghoan1511@gmail.com>
2024-06-01 23:40:26 +0700

    update

--- lib/src/constants/app_key.dart
+++ lib/src/constants/app_key.dart
@@ -1,4 +1,3 @@
 class HAppKey {
-  static String fcmKeyServer =
-      'AAAARTuQXYY:APA91bFGx0nUkYOYiByVhwz6kyeXkJ987N8KaNST_Y030DJBUyn88kunuaZm3mZkV0cMeD7nMtqeugjiI3pDqfIBxyi8rWV33yKN46m1Ym1HCYE5aOeprcH_ZA7IuHdK_t_7LvgCGv7O';
+  static String fcmKeyServer = '';
 }
